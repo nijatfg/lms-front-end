3bef79b7e4c19a6e9585aaef5494143bd3256d24
nijatfg <153588579+nijatfg@users.noreply.github.com>
2024-04-05 20:11:53 +0400

    ViewGroupUsers changed and css file added

--- src/Components/Group/ViewGroupUsers.js
+++ src/Components/Group/ViewGroupUsers.js
@@ -1,38 +1,66 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
+import "./ViewGroupUsers.css";
 
 const ViewGroupUsers = () => {
     const [users, setUsers] = useState([]);
+    const [showUsers, setShowUsers] = useState(false);
+    const [groupName, setGroupName] = useState(''); // State to store the group name
     const groupId = localStorage.getItem('groupId'); // Get groupId from local storage
+    const token = localStorage.getItem("jwtToken");
 
     useEffect(() => {
         if (groupId) {
+            fetchGroupInfo(groupId);
             fetchUsersByGroup(groupId);
         }
-    }, [groupId]); // Fetch users when groupId changes
+    }, [groupId]); // Fetch group info and users when groupId changes
+
+    const fetchGroupInfo = async (groupId) => {
+        try {
+            const response = await axios.get(`http://localhost:8080/api/v1/groups/${groupId}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            setGroupName(response.data.name);
+        } catch (error) {
+            console.error('Error fetching group info:', error);
+        }
+    };
 
     const fetchUsersByGroup = async (groupId) => {
         try {
-            const response = await axios.get(`http://localhost:8080/api/v1/users/groups/${groupId}`);
+            const response = await axios.get(`http://localhost:8080/api/v1/users/groups/${groupId}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
             setUsers(response.data);
         } catch (error) {
             console.error('Error fetching users by group ID:', error);
         }
     };
 
+    const toggleUsersList = () => {
+        setShowUsers(!showUsers);
+    };
+
     return (
-        <div>
-            <h2>Users by Group</h2>
-            <ul>
-                {users.map(user => (
-                    <li key={user.id}>
-                        <p>Username: {user.username}</p>
-                        <p>Email: {user.email}</p>
-                        <p>Role: {user.authorities[0].authority}</p>
-                        {/* Display other user details as needed */}
-                    </li>
-                ))}
-            </ul>
+        <div className="group-users-container">
+            <div className="group-users-header" onClick={toggleUsersList}>
+                <h2>{groupName}</h2>
+                <p>{users.filter(user => user.authorities[0].authority === 'STUDENT').length} Students</p>
+            </div>
+            {showUsers && (
+                <ul className="users-list">
+                    {users.filter(user => user.authorities[0].authority === 'STUDENT').map(user => (
+                        <li key={user.id}>
+                            <p>{user.username}</p>
+                        </li>
+                    ))}
+                </ul>
+            )}
         </div>
     );
 };
