08e50d170f9f998cdeaf112a007a0a91683e06fe
nijatfg <153588579+nijatfg@users.noreply.github.com>
2024-04-05 17:53:54 +0400

    GetMaterialComponent changed and css added

--- src/Components/Material/GetMaterialComponent.js
+++ src/Components/Material/GetMaterialComponent.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import "./GetMaterial.css"
 
 const GetMaterialComponent = () => {
     const [materials, setMaterials] = useState([]);
@@ -34,7 +35,6 @@ const GetMaterialComponent = () => {
                 },
                 responseType: 'blob', // Specify response type as blob
             });
-            // Create a blob URL for the file and open it in a new window
             const blobUrl = URL.createObjectURL(new Blob([response.data]));
             const link = document.createElement('a');
             link.href = blobUrl;
@@ -50,21 +50,24 @@ const GetMaterialComponent = () => {
     return (
         <div className="get-material-container">
             <h2>Uploaded Materials</h2>
-            <ul>
+            <table>
+                <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Type</th>
+                    <th>Download</th>
+                </tr>
+                </thead>
+                <tbody>
                 {materials.map((material) => (
-                    <li key={material.id}>
-                        <strong>Title: {material.title}</strong>
-                        <p>Type: {material.type}</p>
-                        <p>Content: {material.content}</p>
-                        <p>Group ID: {material.groupId}</p>
-                        <p onClick={() => handleDownload(material.content)}
-                           style={{cursor: 'pointer', color: 'blue', textDecoration: 'underline'}}>
-                            {material.content}
-                        </p>
-                        {/* Add more details as needed */}
-                    </li>
+                    <tr key={material.id}>
+                        <td>{material.title}</td>
+                        <td>{material.type}</td>
+                        <td onClick={() => handleDownload(material.content)} className="download-link">Download</td>
+                    </tr>
                 ))}
-            </ul>
+                </tbody>
+            </table>
         </div>
     );
 };
